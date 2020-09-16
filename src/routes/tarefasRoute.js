20e3aac4b923d50d244bb6a84e4cbe5f0ac8e1a3
Paula Allemand <paulaalemand@gmail.com>
2020-09-16 01:35:36 -0300

    first commit

---
+++ src/routes/tarefasRoute.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/tarefasController")
+
+router.get("/", controller.get)
+router.get("/tarefas", controller.get)
+router.get("/:id", controller.getById)
+
+
+module.exports = router
