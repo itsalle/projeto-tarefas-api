2e62ec30b699c3d4d44d7e456c350eb5139067a3
Paula Allemand <paulaalemand@gmail.com>
2020-09-16 17:26:17 -0300

    added comment in app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ const tarefas = require("./routes/tarefasRoute")
 app.use(express.json());
 
 app.use(function (req, res, next) {
+  // Definindo permissão de acesso, o * libera acesso a qualquer site
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   )
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
   next()
 })
 
